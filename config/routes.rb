90425ec25ff1cd695ab9c044c6e97a9d27c6c64e
kenprochnow@gmail.com <kenprochnow@gmail.com>
2018-07-01 20:49:08 -0700

    create expenses

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,15 @@ Rails.application.routes.draw do
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'expenses', :to => "expenses#index"
+  get 'expenses/index', :to => "expenses#index"
+  get 'expenses/show/:id', :to => "expenses#show"
+  get 'expenses/new', :to => "expenses#new"
+  post 'expenses/create', :to => "expenses#create"
+  get 'expenses/edit/:id', :to => "expenses#edit"
+  post 'expenses/update/:id', :to => "expenses#update"
+  get 'expenses/delete/:id', :to => "expenses#delete"
+
+
 end
